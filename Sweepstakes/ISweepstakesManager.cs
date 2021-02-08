d302c44d098cb2cc13a18bd30d774ca4b817e91d
Matthew Alexander <matt.bryantalex@gmail.com>
2021-02-08 08:13:30 -0800

    added ISweepstakesManager interface

---
+++ Sweepstakes/ISweepstakesManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    interface ISweepstakesManager
+    {
+        void insertSweepstakes(Sweepstakes sweepstakes);
+
+        Sweepstakes GetSweepstakes();
+
+    }
+}
